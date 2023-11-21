fa0542c95b61d69b67ede49d533d069563512795
Stronauta <samircastillop@gmail.com>
2023-11-21 00:13:17 -0400

    Se termino el diseño del registro y se realizo eliminar, guardar y nuevo

--- Shared/Ventas.cs
+++ Shared/Ventas.cs
@@ -11,9 +11,10 @@ namespace Samir_API1_P2.Shared
 	{
 		[Key]
 		public int VentaId { get; set; }
-		public DateTime Fecha { get; set; }
+		public DateTime Fecha { get; set; } = DateTime.Now;
 		public int ClienteId { get; set; }
 		public double Monto { get; set; }
 		public double Balance { get; set; }
+		public string? Observaciones { get; set; }
 	}
 }
