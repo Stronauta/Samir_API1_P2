e688863163cb936bab2c37fadc4a22b6650e67ee
Stronauta <samircastillop@gmail.com>
2023-11-20 21:10:06 -0400

    Se crearon los modelos

---
+++ Shared/Ventas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samir_API1_P2.Shared
+{
+	public class Ventas
+	{
+		[Key]
+		public int VentaId { get; set; }
+		public DateTime Fecha { get; set; }
+		public int ClienteId { get; set; }
+		public double Monto { get; set; }
+		public double Balance { get; set; }
+	}
+}
---
+++ Shared/VentasDetalles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samir_API1_P2.Shared
+{
+	public class VentasDetalles
+	{
+		[Key]
+		public int ventasDetailId { get; set; }
+		[ForeignKey("VentaId")]
+		public int VentaId { get; set; }
+	}
+}
