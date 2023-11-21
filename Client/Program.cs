df540b716fa6b4c3cadbfc08a1d2cf3a351cd003
Stronauta <samircastillop@gmail.com>
2023-11-20 23:16:27 -0400

    Se instalo el radzen y se comenzo a trabajar en el registro de la pagina

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using Samir_API1_P2.Client;
 
 namespace Samir_API1_P2.Client
@@ -11,6 +12,7 @@ namespace Samir_API1_P2.Client
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
+			builder.Services.AddScoped<NotificationService>();
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
